b967bd7e53345dd5fa306f5300278e5fd4a35e3d
Garrett Martin <garrett@blvrd.co>
2022-03-28 17:48:05 -0500

    Add better sum_to_n solution that avoids looping

--- assembly-prework/sum_to_n.asm
+++ assembly-prework/sum_to_n.asm
@@ -1,10 +1,8 @@
 section .text
 global sum_to_n
 sum_to_n:
-  xor rax, rax ; make sure rax is 0
-  mov rcx, rdi ; get the argument `n` from the rdi register, put it in rcx
-sum_loop:
-  add rax, rcx ; add the current value of rcx to rax
-  dec rcx      ; decrement rcx
-  jge sum_loop ; execute the loop again if the last comparison was >= 0 (stored in the flags register)
+  mov rax, rdi ; get the argument `n` from the rdi register, put it in rax
+  add rax, 1   ; add 1 to rax
+  mul rdi      ; multiply rax by the original argument for `n`
+  shr rax, 1   ; shift rax right by one bit to divide by two (this blew my mind)
   ret
