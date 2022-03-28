5aa7afaf4a935ee7abb9dc3f6cb727eec605b9ad
Garrett Martin <garrett@blvrd.co>
2022-03-28 17:21:17 -0500

    Naive looping solution for sum_to_n

--- assembly-prework/sum_to_n.asm
+++ assembly-prework/sum_to_n.asm
@@ -1,3 +1,10 @@
 section .text
 global sum_to_n
 sum_to_n:
+  xor rax, rax ; make sure rax is 0
+  mov rcx, rdi ; get the argument `n` from the rdi register, put it in rcx
+sum_loop:
+  add rax, rcx ; add the current value of rcx to rax
+  dec rcx      ; decrement rcx
+  jge sum_loop ; execute the loop again if the last comparison was >= 0 (stored in the flags register)
+  ret
