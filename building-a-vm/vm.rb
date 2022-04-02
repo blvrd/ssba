1c5857648588d120220bdbd47478050a708dcacb
Garrett Martin <garrett@blvrd.co>
2022-04-02 15:19:56 -0500

    Add immediate instruction

--- building-a-vm/vm.rb
+++ building-a-vm/vm.rb
@@ -1,14 +1,23 @@
 # Registers
-PC = 0x00
-R1 = 0x01
-R2 = 0x02
+PC = 0x00 # program counter
+R1 = 0x01 # general-purpose register 1
+R2 = 0x02 # general-purpose register 2
 
 # Instructions
 LOADW  = 0x01
 STOREW = 0x02
 ADD    = 0x03
 SUB    = 0x04
+MUL    = 0x05
+DIV    = 0x06
+ADDIMM = 0x07
+SHR    = 0x08
 HALT   = 0xff
+
+# Memory addresses
+OUTPUT_OFFSET = 0x0e
+INPUT1_OFFSET = 0x10
+INPUT2_OFFSET = 0x12
 
 def load_word(registers, memory, reg, addr)
   registers[reg] = memory[addr] + (memory[addr + 1] * 256)
@@ -29,8 +38,18 @@ def add(registers, reg1, reg2)
   registers[PC] += 3
 end
 
+def add_imm(registers, reg, imm)
+  registers[reg] += imm
+  registers[PC] += 3
+end
+
 def sub(registers, reg1, reg2)
   registers[reg1] -= registers[reg2]
+  registers[PC] += 3
+end
+
+def shift_right(registers, reg, imm)
+  registers[reg] = registers[reg] >> imm
   registers[PC] += 3
 end
 
@@ -41,15 +60,25 @@ end
 def vm(registers, memory)
   while memory[registers[PC]] != HALT
     instruction = memory[registers[PC]]
+    op1         = memory[registers[PC] + 1]
+    op2         = memory[registers[PC] + 2]
+
     case instruction
     when 0x01
-      load_word(registers, memory, memory[registers[PC] + 1], memory[registers[PC] + 2])
+      load_word(registers, memory, op1, op2)
     when 0x02
-      store_word(registers, memory, memory[registers[PC] + 1], memory[registers[PC] + 2])
+      store_word(registers, memory, op1, op2)
     when 0x03
-      add(registers, memory[registers[PC] + 1], memory[registers[PC] + 2])
+      add(registers, op1, op2)
     when 0x04
-      sub(registers, memory[registers[PC] + 1], memory[registers[PC] + 2])
+      sub(registers, op1, op2)
+    when 0x07
+      add_imm(registers, op1, op2)
+    when 0x08
+      shift_right(registers, op1, op2)
+    else
+      puts "Can't execute instruction: 0x#{instruction.to_s(16)}"
+      exit 1
     end
   end
 
@@ -66,15 +95,15 @@ registers = [
 ]
 
 memory = [
-  LOADW, R1, 0x10,
-  LOADW, R2, 0x12,
-  ADD, R1, R2,
-  STOREW, R1, 0x0e,
+  LOADW, R1, INPUT1_OFFSET,
+  LOADW, R2, INPUT2_OFFSET,
+  ADDIMM, R1, 5,
+  STOREW, R1, OUTPUT_OFFSET,
   HALT,
-  0x00,
-  0x00, 0x00,
-  0xa1, 0x14,
-  0x0c, 0x00
+  0x00,  # not used
+  0, 0,  # ouput
+  25, 0, # input 1
+  5, 0   # input 2
 ]
 
 vm(registers, memory)
